731d6649eb1a3a4d593a3a618c40eefdb3cb0b74
NicoHerlan <nicodvkcss@gmail.com>
2023-07-03 23:29:34 -0300

    add render backend config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,9 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://dw-clima.onrender.com"
 
 
 // !Backend hosteado en render
 // export const BASE_URL = "https://dw-clima.onrender.com"
 
+
+
+//http://localhost:3000
